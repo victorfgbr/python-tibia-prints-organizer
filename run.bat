65d427959599b79c81d8e9fee2978d94dbc0a9de
Victor Goulart <victorfgbr@gmail.com>
2023-07-22 20:22:29 -0300

    update screenshot folders on run bash

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off 
 chcp 1252
-set screenshotsFolderPath=C:\Users\Usuário\AppData\Local\Tibia\packages\Tibia\screenshots
+set screenshotsFolderPath=C:\Users\vgoul\AppData\Local\Tibia\packages\Tibia\screenshots
 
 py app.py %screenshotsFolderPath%
 
